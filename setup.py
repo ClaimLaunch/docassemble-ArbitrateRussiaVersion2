4afedf4aa1f5ad62d99c8c85006aa24274ff1286
System Administrator <129937121+ClaimLaunch@users.noreply.github.com>
2023-05-18 04:43:24 -0400

    5.18 - expro and protection claims

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.ArbitrateRussiaVersion2',
-      version='0.0.12',
+      version='0.0.13',
       description=('A docassemble extension.'),
       long_description='Proprietary License\r\n\r\nThis code is proprietary and confidential, and is the exclusive property of David Chriki. No part of this code may be used, modified, or distributed without the express written consent of David Chriki. Unauthorized use, modification, or distribution of this code is strictly prohibited.',
       long_description_content_type='text/markdown',
